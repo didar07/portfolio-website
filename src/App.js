9420a8f52d4f2417c52f6262fa705d1fe0f2d967
didar07 <didarul111015@gmail.com>
2022-12-10 16:57:14 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layout/Main';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Main></Main>
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        }
+      ]
     }
   ])
   return (
