ba87d77389102cb7722e2d0dbc0fc4d6dd9ea0c7
didar07 <didarul111015@gmail.com>
2022-12-11 20:43:52 +0600

    skills added

--- src/Components/GeniusDetails/GeniusDetails.js
+++ src/Components/GeniusDetails/GeniusDetails.js
@@ -3,7 +3,41 @@ import React from 'react';
 const GeniusDetails = () => {
     return (
         <div>
-            <h1>GeniusDetails</h1>
+            <div className='text-center mt-2'>
+                <button className='text-4xl text-center px-3 py-2 shadow-lg font-mono mt-3 text-white btn btn-secondary'>Genius Car</button>
+
+                <div className="card mt-5 bg-base-100 shadow-xl">
+                    <div className='grid grid-cols-3 gap-3'>
+
+                        <a href="https://ibb.co/VYd4hQL"><img src="https://i.ibb.co/B2mM7rf/club1.png" alt="club1" border="0" /></a>
+
+                        <a href="https://ibb.co/MkTTkrc"><img src="https://i.ibb.co/TMXXM64/club2.png" alt="club2" border="0" /></a>
+
+                        <a href="https://ibb.co/x5Njz1P"><img src="https://i.ibb.co/f0sprkB/club3.png" alt="club3" border="0" /></a>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Genius Car
+                            <div className="badge badge-secondary">Project</div>
+                        </h2>
+
+                        <button><a className='btn btn-outline btn-secondary' href="https://genius-car-b3c7e.web.app/">Live Site Link</a></button>
+                        <button><a className='btn btn-outline btn-secondary' href="https://github.com/didar07/genius-car-client">Client Site Code Link</a></button>
+                        <button><a className='btn btn-outline btn-secondary' href="https://github.com/didar07/genius-car-server">Server Site Code Link</a></button>
+
+                        <p className='font-bold'>these features are using this projects</p>
+                        <div className="card-actions justify-center">
+                            <div className="badge badge-outline">React</div>
+                            <div className="badge badge-outline">Firebase</div>
+                            <div className="badge badge-outline">Daisy Ui</div>
+                            <div className="badge badge-outline">Mamba Ui</div>
+                            <div className="badge badge-outline">Mongo Db</div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,37 +7,88 @@ const Home = () => {
     return (
         <div>
             <section>
+                <h2 className='text-3xl font-bold text-center mt-3 text-secondary'>My Skills</h2>
+                <div className='grid grid-cols-3 gap-3'>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse4.mm.bing.net/th?id=OIP.PHBTJoshbg880IH9z_PB6QHaHa&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 80 }}>80%</div>
+                    </div>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse3.mm.bing.net/th?id=OIP.9nmpe21kOln2OBgSM-TDZAAAAA&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 100 }}>100%</div>
+                    </div>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse4.mm.bing.net/th?id=OIP.p86-uZmTpATgZr93QmtYjAHaHa&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 95 }}>95%</div>
+                    </div>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse2.mm.bing.net/th?id=OIP.2lPcNBjKmc6P7DhGJ02cuAHaHa&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 90 }}>90%</div>
+                    </div>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse3.mm.bing.net/th?id=OIP.GgMjcyR95L35NoosfTb9vQAAAA&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 95 }}>95%</div>
+                    </div>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse1.mm.bing.net/th?id=OIP.Ic719f-PCoMe0WkukecVSgAAAA&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 85 }}>85%</div>
+                    </div>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse4.mm.bing.net/th?id=OIP.gJbMaH_nIJYvtkAv6Rn5_QHaHa&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 40 }}>40%</div>
+                    </div>
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse1.mm.bing.net/th?id=OIP.2CmjOPI5qbo_ffqHS-pBjwHaHa&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 60 }}>60%</div>
+                    </div>
+
+                    <div className='shadow-lg'>
+                        <img className='h-60' src="https://tse2.explicit.bing.net/th?id=OIP._yfjgiojxtfaezQcjuGtcwAAAA&pid=Api&P=0" alt="" />
+                        <div className="radial-progress text-primary" style={{ "--value": 30 }}>30%</div>
+                    </div>
+
+                </div>
+            </section>
+
+            <section>
                 <h2 className='text-3xl font-bold text-secondary text-center'>Projects</h2>
                 <div className='grid grid-cols-3 mt-5 gap-5'>
                     <div className='bg-base-200 rounded-lg shadow-lg'>
                         <h2 className='font-bold'>project Name: Snap Kitchen</h2>
                         <img className='p-2 rounded-3xl' src="https://tse2.mm.bing.net/th?id=OIP.-c62OEBsOI4Y19qj85l1lgHaEK&pid=Api&P=0" alt="" />
-                        <Link to='/snapdetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link>
+                        <div className='mt-5 mb-5 text-center'>
+                            <Link to='/snapdetails' className='px-3 py-2 mb-4 mt-5 rounded btn-outline btn-secondary'>view details</Link>
+                        </div>
                     </div>
                     <div className='bg-base-200 rounded-lg shadow-lg'>
                         <h2 className='font-bold'>project Name: Edu-Aid Website</h2>
                         <img className='p-2 rounded-3xl' src="https://t4.ftcdn.net/jpg/02/27/81/85/240_F_227818569_WKUTIgwnHK9UxntN1HiDQGmkSiU9AELX.jpg" alt="" />
-                        <Link to='/edudetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link>
+                        <div className='mt-5 mb-5 text-center'>
+                            <Link to='/edudetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link></div>
                     </div>
                     <div className='bg-base-200 rounded-lg shadow-lg'>
                         <h2 className='font-bold'>project Name: Cars Direct</h2>
                         <img className='p-2 rounded-3xl' src="https://tse3.mm.bing.net/th?id=OIP.yvZff2Ry0WxI0vTlvUTd1QHaEK&pid=Api&P=0" alt="" />
-                        <Link to='/carsdetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link>
+                        <div className='mt-5 mb-5 text-center'>
+                            <Link to='/carsdetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link></div>
                     </div>
                     <div className='bg-base-200 rounded-lg shadow-lg'>
                         <h2 className='font-bold'>project Name: Quiz World App</h2>
                         <img className='p-2 rounded-3xl' src="https://img.freepik.com/free-psd/pink-3d-question-engagement-interface-mockup_106244-1773.jpg?size=626&ext=jpg&uid=R87258934&ga=GA1.2.188054106.1669221212&semt=sph" alt="" />
-                        <Link to='/quizdetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link>
+                        <div className='mt-5 mb-5 text-center'>
+                            <Link to='/quizdetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link></div>
                     </div>
                     <div className='bg-base-200 rounded-lg shadow-lg'>
                         <h2 className='font-bold'>project Name: One Man Club</h2>
                         <img className='p-2 rounded-3xl' src="https://tse2.mm.bing.net/th?id=OIP.6MYylbBeXCwF6Kyj4CUsqgHaE8&pid=Api&P=0" alt="" />
-                        <Link to='/onemandetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link>
+                        <div className='mt-5 mb-5 text-center'>
+                            <Link to='/onemandetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link></div>
                     </div>
                     <div className='bg-base-200 rounded-lg shadow-lg'>
                         <h2 className='font-bold'>project Name: Genius Car Project</h2>
                         <img className='p-2 rounded-3xl' src="https://img.freepik.com/free-photo/black-sport-car-front-side-salon-view-black-wheel-with-metallic-silver-color-direction-door-open_114579-1166.jpg?size=626&ext=jpg&uid=R87258934&ga=GA1.2.188054106.1669221212&semt=sph" alt="" />
-                        <Link to='/geniusdetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link>
+                        <div className='mt-5 mb-5 text-center'>
+                            <Link to='/geniusdetails' className='px-3 py-2 mb-4 mt-2 rounded btn-outline btn-secondary'>view details</Link></div>
                     </div>
                 </div>
             </section>
